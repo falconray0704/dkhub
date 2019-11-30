5d2e4285176584e9ca223cc13619b5ec8b650001
FalconRay <falconray@yahoo.com>
2019-11-30 17:56:23 +0800

    Refine the installation path of dnscrypt-proxy and make release to relPkgs.

--- dnscryptProxy/run.sh
+++ dnscryptProxy/run.sh
@@ -12,21 +12,27 @@ set -e
 LATEST_VERSION="2.0.33"
 ARCH=$(arch)
 
-TOP_DIR=$(cd ../ && pwd -P)
+#TOP_PATH=$(cd ../ && pwd -P)
+TOP_PATH=$(dirname ${PWD})
 
-BUILD_ROOT_DIR=${TOP_DIR}/dnscryptProxy/buildRoot
+BUILD_ROOT_PATH=${TOP_PATH}/dnscryptProxy/buildRoot
 
-GOPATH_DIR=${BUILD_ROOT_DIR}/gopath
+GOPATH_PATH=${BUILD_ROOT_PATH}/gopath
 
-SRC_ROOT_DIR=${BUILD_ROOT_DIR}/src
-SRC_BUILD_DIR=${BUILD_ROOT_DIR}/src/dnscrypt-proxy-${LATEST_VERSION}/dnscrypt-proxy
+SRC_ROOT_PATH=${BUILD_ROOT_PATH}/src
+SRC_BUILD_PATH=${BUILD_ROOT_PATH}/src/dnscrypt-proxy-${LATEST_VERSION}/dnscrypt-proxy
 
-DEST_DIR=${BUILD_ROOT_DIR}/dest/dnscrypt-proxy-${ARCH}
+DEST_PATH=${BUILD_ROOT_PATH}/dest/dnscrypt-proxy-${ARCH}
+
+RELEASE_ROOT_DIR="deployPkgs"
+RELEASE_PATH=${TOP_PATH}/${RELEASE_ROOT_DIR}/${ARCH}
+
+INSTALL_ROOT_PATH=${HOME}/${RELEASE_ROOT_DIR}
 
 get_latest_src_func()
 {
-    mkdir -p ${SRC_ROOT_DIR}
-    pushd ${SRC_ROOT_DIR}
+    mkdir -p ${SRC_ROOT_PATH}
+    pushd ${SRC_ROOT_PATH}
     rm -rf dnscrypt-proxy-src.${LATEST_VERSION}.tar.gz
     wget -c https://github.com/jedisct1/dnscrypt-proxy/archive/${LATEST_VERSION}.tar.gz
 #    mv ${LATEST_VERSION}.tar.gz dnscrypt-proxy-src.${LATEST_VERSION}.tar.gz
@@ -36,29 +42,29 @@ get_latest_src_func()
 build_latest_func()
 {
 
-    mkdir -p ${GOPATH_DIR}/bin
-    mkdir -p ${GOPATH_DIR}/src
+    mkdir -p ${GOPATH_PATH}/bin
+    mkdir -p ${GOPATH_PATH}/src
 
-    rm -rf ${SRC_BUILD_DIR}/dnscrypt-proxy-${ARCH}
-    rm -rf ${SRC_ROOT_DIR}/dnscrypt-proxy-${LATEST_VERSION}
-    tar -zxf ${SRC_ROOT_DIR}/${LATEST_VERSION}.tar.gz -C ${SRC_ROOT_DIR}/
+    rm -rf ${SRC_BUILD_PATH}/dnscrypt-proxy-${ARCH}
+    rm -rf ${SRC_ROOT_PATH}/dnscrypt-proxy-${LATEST_VERSION}
+    tar -zxf ${SRC_ROOT_PATH}/${LATEST_VERSION}.tar.gz -C ${SRC_ROOT_PATH}/
 
-#    docker run --rm -it -v ${TOP_DIR}:${TOP_DIR} -v ${GOPATH_DIR}:/gopath -w ${SRC_BUILD_DIR} --entrypoint="/bin/bash" --env GOPATH=/gopath golang 
-    docker run --rm -it -v ${TOP_DIR}:${TOP_DIR} -v ${GOPATH_DIR}:/gopath -w ${SRC_BUILD_DIR} --env GOPATH=/gopath golang go build -ldflags="-s -w" -o dnscrypt-proxy-${ARCH}
-#    docker run --rm -it -v ${TOP_DIR}:${TOP_DIR} -v ${GOPATH_DIR}:/gopath -w ${SRC_BUILD_DIR} --env GOPATH=/gopath --dns="192.168.11.1" golang go build -ldflags="-s -w" -o dnscrypt-proxy-${ARCH}
+#    docker run --rm -it -v ${TOP_PATH}:${TOP_PATH} -v ${GOPATH_PATH}:/gopath -w ${SRC_BUILD_PATH} --entrypoint="/bin/bash" --env GOPATH=/gopath golang 
+    docker run --rm -it -v ${TOP_PATH}:${TOP_PATH} -v ${GOPATH_PATH}:/gopath -w ${SRC_BUILD_PATH} --env GOPATH=/gopath golang go build -ldflags="-s -w" -o dnscrypt-proxy-${ARCH}
+#    docker run --rm -it -v ${TOP_PATH}:${TOP_PATH} -v ${GOPATH_PATH}:/gopath -w ${SRC_BUILD_PATH} --env GOPATH=/gopath --dns="192.168.11.1" golang go build -ldflags="-s -w" -o dnscrypt-proxy-${ARCH}
 
 
-    rm -rf ${DEST_DIR}
-    mkdir -p ${DEST_DIR}
-    cp ${SRC_BUILD_DIR}/dnscrypt-proxy-${ARCH} ${DEST_DIR}/dnscrypt-proxy
-    cp ${SRC_BUILD_DIR}/example-* ${DEST_DIR}/
+    rm -rf ${DEST_PATH}
+    mkdir -p ${DEST_PATH}
+    cp ${SRC_BUILD_PATH}/dnscrypt-proxy-${ARCH} ${DEST_PATH}/dnscrypt-proxy
+    cp ${SRC_BUILD_PATH}/example-* ${DEST_PATH}/
 
 }
 
 config_func()
 {
 
-    pushd ${HOME}/dnscrypt-proxy
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
     cp example-dnscrypt-proxy.toml dnscrypt-proxy.toml
 	sed -i "s/^# server_names =.*/server_names = \['cisco', 'google', 'scaleway-fr', 'yandex', 'cloudflare'\]/" dnscrypt-proxy.toml
 	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53'\]/" dnscrypt-proxy.toml
@@ -79,14 +85,14 @@ install_service_func()
 	sudo sed -i '/^static domain_name_servers=.*/d' /etc/dhcpcd.conf
 	sudo sed -i '/^#static domain_name_servers=192.168.1.1$/a\static domain_name_servers=127.0.0.1' /etc/dhcpcd.conf
 
-    pushd ${HOME}/dnscrypt-proxy
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
     sudo ./dnscrypt-proxy -service install
     popd
 }
 
 uninstall_service_func()
 {
-    pushd ${HOME}/dnscrypt-proxy
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
     sudo ./dnscrypt-proxy -service stop
     sudo ./dnscrypt-proxy -service uninstall
     popd
@@ -96,16 +102,16 @@ uninstall_service_func()
 
 usage_func()
 {
-    echo "./build.sh <cmd> <target>"
+    echo "./run.sh <cmd> <target>"
     echo ""
     echo "Supported cmd:"
-    echo "[ get, build, install, uninstall ]"
+    echo "[ get, build, release, install, uninstall ]"
     echo ""
     echo "Supported target:"
     echo "[ src, dns, service, installer ]"
 }
 
-echoG "TOP_DIR:${TOP_DIR}"
+echoG "TOP_PATH:${TOP_PATH}"
 
 [ $# -lt 2 ] && echo "Invalid args count:$# " && usage_func && exit 1
 
@@ -119,7 +125,7 @@ case $1 in
             echoR "Unknow target:$2, only support fetching target [ src ]."
         fi
         ;;
-    build) echoY "Building dynamic SS from github ..."
+    build) echoY "Building ..."
         if [ $2 == "dns" ]
         then
             echo "Building dnscrypt-proxy ${LATEST_VERSION}.tar.gz..."
@@ -129,24 +135,41 @@ case $1 in
             echo "Building dnscrypt-proxy docker image for deployment..."
             docker rmi -f rayruan/dnscrypt-proxy_${ARCH}:installer
             docker image prune
-            docker build --rm -t rayruan/dnscrypt-proxy_${ARCH}:installer -f Dockerfile_installer.img ${DEST_DIR}
+            docker build --rm -t rayruan/dnscrypt-proxy_${ARCH}:installer -f Dockerfile_installer.img ${DEST_PATH}
         else
             echoR "Unknow target:$2, only support building target [dns, installer]."
+        fi
+        ;;
+    release) echoY "Releasing ..."
+        if [ $2 == "dns" ]
+        then
+            echoY "Releasing dnscrypt-proxy to:${RELEASE_PATH}"
+
+            mkdir -p ${RELEASE_PATH}
+            rm -rf ${RELEASE_PATH}/dnscrypt-proxy-${ARCH}
+
+            pushd ${RELEASE_PATH}
+            cp -a ${DEST_PATH} ./
+            mv dnscrypt-proxy-${ARCH} dnscrypt-proxy
+            popd
+        else
+            echoR "Unknow target:$2, only support releasing target [ dns ]."
         fi
         ;;
     install) echoY "Installing..."
         if [ $2 == "dns" ] 
         then
-            echoY "Installing dnscrypt-proxy to your ${HOME}..."
-            sudo rm -rf ${HOME}/dnscrypt-proxy
-            docker run --rm -it -v ${HOME}:/target rayruan/dnscrypt-proxy_${ARCH}:installer 
-	        USER_NAME=$(whoami)
+            echoY "Installing dnscrypt-proxy to your ${INSTALL_ROOT_PATH}..."
+            sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+            docker run --rm -it -v ${INSTALL_ROOT_PATH}:/target rayruan/dnscrypt-proxy_${ARCH}:installer 
+	        USER_NAME=$(id -un)
+	        GROUP_NAME=$(id -gn)
 	        #echo "### ${USER_NAME}"
-            sudo chown -hR ${USER_NAME}:${USER_NAME} ${HOME}/dnscrypt-proxy
-            cp dnsCryptSrc/public-resolvers.md* ${HOME}/dnscrypt-proxy/
+            sudo chown -hR ${USER_NAME}:${GROUP_NAME} ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+            cp dnsCryptSrc/public-resolvers.md* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
 
             config_func
-	        cp ./dnsCryptSrc/public* ${HOME}/dnscrypt-proxy/
+	        cp ./dnsCryptSrc/public* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
         elif [ $2 == "service" ]
         then
             echoY "Installing dnscrypt-proxy service..."
@@ -160,7 +183,7 @@ case $1 in
         if [ $2 == "dns" ] 
         then
             echoY "Uinstalling dnscrypt-proxy service ..."
-            sudo rm -rf ${HOME}/dnscrypt-proxy
+            sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
         elif [ $2 == "service" ]
         then
             echoY "Installing dnscrypt-proxy service..."
