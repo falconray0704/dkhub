50cee1c5f229d9a19f056431f1132e4f1929e3b7
FalconRay <falconray@yahoo.com>
2019-12-04 18:04:50 +0800

    Moved  dnscrypt-proxy's deployment operations to sysCfgs reop. Only make building and release here.

--- dnscryptProxy/run.sh
+++ dnscryptProxy/run.sh
@@ -27,8 +27,6 @@ DEST_PATH=${BUILD_ROOT_PATH}/dest/dnscrypt-proxy-${ARCH}
 RELEASE_ROOT_DIR="deployPkgs"
 RELEASE_PATH=${TOP_PATH}/${RELEASE_ROOT_DIR}/${ARCH}
 
-INSTALL_ROOT_PATH=${HOME}/${RELEASE_ROOT_DIR}/${ARCH}
-
 get_latest_src_func()
 {
     mkdir -p ${SRC_ROOT_PATH}
@@ -61,81 +59,34 @@ build_latest_func()
 
 }
 
-config_func()
+build_installer_image_func()
 {
-
-    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-    cp example-dnscrypt-proxy.toml dnscrypt-proxy.toml
-	sed -i "s/^# server_names =.*/server_names = \['cisco', 'google', 'scaleway-fr', 'yandex', 'cloudflare'\]/" dnscrypt-proxy.toml
-	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53'\]/" dnscrypt-proxy.toml
-	sed -i "s/.*ignore_system_dns =.*/ignore_system_dns = true/" dnscrypt-proxy.toml
-	sed -i "s/.*force_tcp =.*/force_tcp = true/" dnscrypt-proxy.toml
-	sed -i "s/^timeout =.*/timeout = 3000/" dnscrypt-proxy.toml
-
-    echoY "Downloading public-resolvers..."
-    set +e
-    ./dnscrypt-proxy
-    set -e
-
-    popd
+    docker rmi -f rayruan/dnscrypt-proxy_${ARCH}:installer
+    docker image prune
+    docker build --rm -t rayruan/dnscrypt-proxy_${ARCH}:installer -f Dockerfile_installer.img ${DEST_PATH}
 }
 
-install_from_docker_installer_func()
-{
-    echoY "Installing dnscrypt-proxy to your ${INSTALL_ROOT_PATH}..."
-    sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-    mkdir -p ${INSTALL_ROOT_PATH}
-    docker run --rm -it -v ${INSTALL_ROOT_PATH}:/target rayruan/dnscrypt-proxy_${ARCH}:installer 
-    USER_NAME=$(id -un)
-    GROUP_NAME=$(id -gn)
-    #echo "### ${USER_NAME}"
-    sudo chown -hR ${USER_NAME}:${GROUP_NAME} ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-    cp dnsCryptSrc/public-resolvers.md* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
-
-    config_func
-    cp ./dnsCryptSrc/public* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
-}
-
-install_service_func()
-{
-	sudo sed -i '/^static domain_name_servers=.*/d' /etc/dhcpcd.conf
-	sudo sed -i '/^#static domain_name_servers=192.168.1.1$/a\static domain_name_servers=127.0.0.1' /etc/dhcpcd.conf
-
-    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-    sudo ./dnscrypt-proxy -service install
-    popd
-}
-
-uninstall_service_func()
-{
-    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-    sudo ./dnscrypt-proxy -service stop
-    sudo ./dnscrypt-proxy -service uninstall
-    popd
-
-	sudo sed -i '/^static domain_name_servers=.*/d' /etc/dhcpcd.conf
-}
 
 usage_func()
 {
-    echo "./run.sh <cmd> <target>"
+    echoY "./run.sh <cmd> <target>"
     echo ""
-    echo "Supported cmd:"
-    echo "[ get, build, release, init, install, uninstall ]"
+    echoY "Supported cmd:"
+    echo "[ get, build, release ]"
     echo ""
-    echo "Supported target:"
-    echo "[ src, dns, service, installer ]"
+    echoY "Supported target:"
+    echo "[ src, dns ]"
 }
 
 echoG "TOP_PATH:${TOP_PATH}"
 
-[ $# -lt 2 ] && echo "Invalid args count:$# " && usage_func && exit 1
+[ $# -lt 2 ] && echoR "Invalid args count:$# " && usage_func && exit 1
 
 case $1 in
     get) echoY "Fetching $2 ..."
         if [ $2 == "src" ]
         then
-            echo "Fetching dnscrypt-proxy  ${LATEST_VERSION}.tar.gz"
+            echoY "Fetching dnscrypt-proxy  ${LATEST_VERSION}.tar.gz"
             get_latest_src_func
         else
             echoR "Unknow target:$2, only support fetching target [ src ]."
@@ -144,14 +95,13 @@ case $1 in
     build) echoY "Building ..."
         if [ $2 == "dns" ]
         then
-            echo "Building dnscrypt-proxy ${LATEST_VERSION}.tar.gz..."
+            echoY "Building dnscrypt-proxy ${LATEST_VERSION}.tar.gz..."
             build_latest_func
         elif [ $2 == "installer" ]
         then
-            echo "Building dnscrypt-proxy docker image for deployment..."
-            docker rmi -f rayruan/dnscrypt-proxy_${ARCH}:installer
-            docker image prune
-            docker build --rm -t rayruan/dnscrypt-proxy_${ARCH}:installer -f Dockerfile_installer.img ${DEST_PATH}
+#            echoY "Building dnscrypt-proxy docker image for deployment..."
+#            build_installer_image_func
+            echoR "Unsupport build docker installer now."
         else
             echoR "Unknow target:$2, only support building target [dns, installer]."
         fi
@@ -171,42 +121,6 @@ case $1 in
         else
             echoR "Unknow target:$2, only support releasing target [ dns ]."
         fi
-        ;;
-    init) echoY "Initiating configs of $2"
-        if [ $2 == "dns" ]
-        then
-            config_func
-        else
-            echoR "Unknow target:$2, only support init targets [ dns ]."
-        fi
-        ;;
-    install) echoY "Installing..."
-        if [ $2 == "dns" ] 
-        then
-            #install_from_docker_installer_func
-            echoY "Unsupport install from docker installer now, please install relPkgs."
-        elif [ $2 == "service" ]
-        then
-            echoY "Installing dnscrypt-proxy service..."
-            install_service_func
-        else
-            echoR "Unknow target:$2, only support installing targets [dns, service]."
-        fi
-        echoG "Install $2 finished."
-        ;;
-    uninstall) echoY "Uninstalling..."
-        if [ $2 == "dns" ] 
-        then
-            echoY "Uinstalling dnscrypt-proxy service ..."
-            sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-        elif [ $2 == "service" ]
-        then
-            echoY "Installing dnscrypt-proxy service..."
-            uninstall_service_func
-        else
-            echoR "Unknow target:$2, only support uninstalling targets [dns, service]."
-        fi
-        echoG "Uninstall $2 finished."
         ;;
     *) echo "Unsupported cmd:$1."
         usage_func
