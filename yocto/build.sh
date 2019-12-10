d6d810d798f727859577b56fd69823065eed514d
FalconRay <falconray@yahoo.com>
2019-12-10 17:05:16 +0800

    Install realpath for yocto build environment, and refined build.sh of yocto.

--- yocto/build.sh
+++ yocto/build.sh
@@ -3,6 +3,9 @@
 set -e
 #set -x
 
+. ../libShell/echo_color.lib
+. ../libShell/sysEnv.lib
+
 
 build_img_func()
 {
@@ -18,14 +21,14 @@ build_img_func()
     elif [ ${TARGET} == build ]; then
         sed -i "s/yocto/yocto_${ARCH}/" ./Dockerfile_${TARGET}${TARGET_VER}.img.${ARCH}
     else
-            echo "Unsupport target:${TARGET} for image building."
+            echoR "Unsupport target:${TARGET} for image building."
             exit 1
     fi
 
     docker build --rm -t rayruan/yocto_${ARCH}:${TARGET}${TARGET_VER} \
-        --build-arg "group=${USER}" \
+        --build-arg "group=$(id -gn)" \
         --build-arg "gid=$(id -u)" \
-        --build-arg "user=${USER}" \
+        --build-arg "user=$(id -un)" \
         --build-arg	"uid=$(id -g)" \
         -f ./Dockerfile_${TARGET}${TARGET_VER}.img.${ARCH} .
 }
@@ -39,7 +42,7 @@ build_target_func()
         do_clean_img_func ${TARGET} ${TARGET_VER}
         build_img_func ${TARGET} ${TARGET_VER}
         ;;
-        *) echo "Unsupported version:$2."
+        *) echoR "Unsupported version:$2."
         exit 1
     esac
 }
@@ -57,28 +60,28 @@ ARCH=$(arch)
 
 usage_func()
 {
-    echo "./build.sh <target> <target version>"
+    echoY "./build.sh <target> <target version>"
     echo ""
-    echo "Supported target:"
+    echoY "Supported target:"
     echo "[ basic, build ]"
     echo ""
-    echo "Supported target version:"
+    echoY "Supported target version:"
     echo "[ 1404, 1604, 1804 ]"
 }
 
-[ $# -lt 2 ] && echo "Invalid args count:$# " && usage_func && exit 1
+[ $# -lt 2 ] && echoR "Invalid args count:$# " && usage_func && exit 1
 
 case $1 in
-    basic|build) echo "Building image rayruan/yocto_${ARCH}:$1$2 ..."
+    basic|build) echoY "Building image rayruan/yocto_${ARCH}:$1$2 ..."
         build_target_func $1 $2
         ;;
-    cleanBasic) echo "Removing image rayruan/yocto_${ARCH}:basic$2 ..."
+    cleanBasic) echoY "Removing image rayruan/yocto_${ARCH}:basic$2 ..."
         do_clean_img_func basic $2
         ;;
-    cleanBuild) echo "Removing image rayruan/yocto_${ARCH}:build$2 ..."
+    cleanBuild) echoY "Removing image rayruan/yocto_${ARCH}:build$2 ..."
         do_clean_img_func build $2
         ;;
-    *) echo "Unsupported target:$1."
+    *) echoR "Unsupported target:$1."
         usage_func
         exit 1
 esac
